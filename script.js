53cecd092d13d755d3cf21cea3325f3b4008e2a2
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-03-18 21:20:44 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let menu_responsive = document.querySelector(".checkbtn");
+
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
+
+let cdsac = 545;
+
+
+
+
+// Rotate my phote
+let myPhoto = document.getElementById('myPhoto');
+
+
+function increaseWidth(){
+  myPhoto.style.width = '300px';
+  
+};
+
+function decreaseWidth(){
+  myPhoto.style.width = '250px';
+}
+
+myPhoto.addEventListener('mouseover',increaseWidth );
+myPhoto.addEventListener('mouseout',decreaseWidth);
+
+
+function cambiarNombre(nombre, ciudad, gusto){
+  let contenido = "Hola mundo";
+  return contenido
+}
